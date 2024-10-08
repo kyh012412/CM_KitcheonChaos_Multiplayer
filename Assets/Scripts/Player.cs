7fae007855428c0419cebc1795587692c47f73ec
kyh012412 <kyh0124.12@gmail.com>
2024-10-08 12:12:05 +0900

    Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -34,10 +36,15 @@ public class Player : MonoBehaviour
         // transform.rotation 이것을 쓰게되면 Quaternion어쩌고 를 계산해야하고 복잡하므로 아래 방법을 사용
         // transform.forward = moveDir;
 
+        isWalking = moveDir != Vector3.zero;
         // 위 처럼 코드 작성시 너무 180회전이 스무스하지 않으므로 아래 코드 사용
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
+    }
+
+    public bool IsWalking(){
+        return isWalking;
     }
 
 
