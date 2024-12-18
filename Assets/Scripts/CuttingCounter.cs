4a152d9470a13c6feb95e0dfb0cf686daff698a5
kyh012412 <kyh0124.12@gmail.com>
2024-12-18 19:17:37 +0900

    Cutting Recipe SO

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] CuttingRecipeSOArray;
     public override void Interact(Player player){
         if(!HasKitchenObject()){
             // There is no KitchenObject here
             if(player.HasKitchenObject()){
                 // Player is carrying something
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if(HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO())){
+                    // Player carrying something that can be Cut
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
             }else{
                 // Player not carrying anything
             }
@@ -27,11 +31,35 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        if(HasKitchenObject()){
-            // There is a KitchenObject here
+        if(HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO())){
+            // There is a KitchenObject here AND it can be cut
+            KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+            // if (outputKitchenObjectSO == null) return;
+
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO){
+        foreach(CuttingRecipeSO cuttingRecipeSO in CuttingRecipeSOArray){
+            if(cuttingRecipeSO.input == inputKitchenObjectSO){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO){
+        foreach(CuttingRecipeSO cuttingRecipeSO in CuttingRecipeSOArray){
+            if(cuttingRecipeSO.input == inputKitchenObjectSO){
+                return cuttingRecipeSO.output;
+            }
+        }
+        // Debug.LogError("input에 해당하는 sliced output이 감지되지 않았음!");
+        Debug.Log("input에 해당하는 sliced output이 감지되지 않았음!");
+        return null;
     }
 }
