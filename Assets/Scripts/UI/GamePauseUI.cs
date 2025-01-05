0470f2f9f3b831f0b9b96fecfa4dc3d0fd0a23a2
kyh012412 <kyh0124.12@gmail.com>
2025-01-05 10:40:53 +0900

    Options, Audio Levels

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class GamePauseUI : MonoBehaviour
 {
     [SerializeField] private Button resumeButton;
+    [SerializeField] private Button optionsButton;
     [SerializeField] private Button mainMenuButton;
 
     private void Awake()
@@ -17,6 +18,10 @@ public class GamePauseUI : MonoBehaviour
 
         mainMenuButton.onClick.AddListener(()=>{
             Loader.Load(Loader.Scene.MainMenuScene);
+        });
+
+        optionsButton.onClick.AddListener(()=>{
+            OptionsUI.Instance.Show();
         });
     }
 
