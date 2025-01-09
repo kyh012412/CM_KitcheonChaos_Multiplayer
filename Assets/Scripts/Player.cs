9d6391c62ea64c582e624a9f5c896a28e87878e1
kyh012412 <kyh0124.12@gmail.com>
2025-01-09 21:06:01 +0900

    Sync Player Movement, Client Vs Server Auth

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -58,12 +58,69 @@ public class Player : NetworkBehaviour ,IKitchenObjectParent
     }
 
     private void Update(){
-        HandleMovement();
+        if(!IsOwner){
+            return;
+        }
+
+        // HandleMovementServerAuth(); // server Auth 방법으로 client는 딜레이가 있는것처럼 느껴지며 서버가 소스의 원천이 된다.
+        HandleMovement(); // Client 에서 각자 움직이며 상대방화면에서 늦게 반영이 된다.
         HandleInteractions();
     }
 
     public bool IsWalking(){
         return isWalking;
+    }
+
+    private void HandleMovementServerAuth(){
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        HandleMovementServerRpc(inputVector);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void HandleMovementServerRpc(Vector2 inputVector){
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDir,moveDistance);
+
+        if(!canMove){
+            // Cannot move towards moveDir
+
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirX,moveDistance);
+
+            if (canMove){
+                // Can move only on the X
+                moveDir = moveDirX;
+            }else{
+                // Cannot move only on the X
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirZ,moveDistance);
+                if(canMove){
+                    // Can move only on the Z
+                    moveDir = moveDirZ;
+                }else{
+                    // Cannot move in any direction
+                }
+            }
+        }
+        if(canMove){
+            transform.position += moveDir * moveSpeed * Time.deltaTime ;
+        }
+
+        // transform.rotation 이것을 쓰게되면 Quaternion어쩌고 를 계산해야하고 복잡하므로 아래 방법을 사용
+        // transform.forward = moveDir;
+
+        isWalking = moveDir != Vector3.zero;
+        // 위 처럼 코드 작성시 너무 180회전이 스무스하지 않으므로 아래 코드 사용
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
     private void HandleMovement(){
